fa229e9937e2ea3688471fdb20f69ef44b40eb73
rgerm <germ@ralphgerm.de>
2011-08-29 12:07:44 +0200

    Header correction

--- src/main/java/org/movsim/simulator/vehicles/longmodel/equilibrium/impl/EquilibriumKrauss.java
+++ src/main/java/org/movsim/simulator/vehicles/longmodel/equilibrium/impl/EquilibriumKrauss.java
@@ -1,17 +1,33 @@
 /**
+ * Copyright (C) 2010, 2011 by Arne Kesting, Martin Treiber,
+ *                             Ralph Germ, Martin Budden
+ *                             <info@movsim.org>
+ * ----------------------------------------------------------------------
  * 
- * Copyright (C) 2010 by Ralph Germ (http://www.ralphgerm.de)
- * 
+ *  This file is part of 
+ *  
+ *  MovSim - the multi-model open-source vehicular-traffic simulator 
+ *
+ *  MovSim is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MovSim is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MovSim.  If not, see <http://www.gnu.org/licenses/> or
+ *  <http://www.movsim.org>.
+ *  
+ * ----------------------------------------------------------------------
  */
 package org.movsim.simulator.vehicles.longmodel.equilibrium.impl;
 
-import org.movsim.simulator.vehicles.longmodel.accelerationmodels.impl.Gipps;
 import org.movsim.simulator.vehicles.longmodel.accelerationmodels.impl.Krauss;
 
-/**
- * @author ralph
- *
- */
 public class EquilibriumKrauss extends EquilibriumPropertiesImpl{
     /**
      * Instantiates a new equilibrium gipps.
