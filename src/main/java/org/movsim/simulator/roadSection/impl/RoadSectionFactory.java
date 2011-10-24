81002583c5ba843c53fd92f5e2986368b3375ae6
rgerm <germ@ralphgerm.de>
2011-10-24 14:42:27 +0200

    Every 100th roadID is a mainRoad

--- src/main/java/org/movsim/simulator/roadSection/impl/RoadSectionFactory.java
+++ src/main/java/org/movsim/simulator/roadSection/impl/RoadSectionFactory.java
@@ -10,23 +10,23 @@ import org.slf4j.LoggerFactory;
 public class RoadSectionFactory {
     /** The Constant logger. */
     final static Logger logger = LoggerFactory.getLogger(RoadSectionFactory.class);
-    
-    
-    public static RoadSection create(InputDataImpl inputData, final RoadInput roadInput, final VehicleGenerator vehGenerator){
+
+    public static RoadSection create(InputDataImpl inputData, final RoadInput roadInput,
+            final VehicleGenerator vehGenerator) {
         final long roadId = roadInput.getId();
-        // 
-        if ((roadInput.getId() > 0)) {
+        if (roadId % 100 == 0) {
+            logger.info("create MAINROAD for road id={}", roadId);
+            return new RoadSectionImpl(inputData, roadInput, vehGenerator);
+        } else if (roadId > 0) {
             logger.info("create ONRAMP for road id={}", roadId);
             // merging from onramp only to most-right lane (shoulder lane)
             return new OnrampMobilImpl(roadInput, vehGenerator, inputData.getProjectMetaDataImpl().getProjectName());
         }
         // quick hack for considering offramp (identified by negative id)
-        else 
-        if ((roadInput.getId() < 0)) {
+        else if (roadId < 0) {
             logger.info("create OFFRAMP for road id={}", roadId);
             return new OfframpImpl(roadInput);
-        }
-        else{ 
+        } else {
             logger.info("create MAINROAD for road id={}", roadId);
             return new RoadSectionImpl(inputData, roadInput, vehGenerator);
         }
