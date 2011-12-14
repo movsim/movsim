9e42cc8f9bead78c972bd786eb1c74cd5dd3c6b0
ralph <germ@ralphgerm.de>
2011-12-14 13:46:37 +0100

    Detector file output for all lanes, then per lane.

--- src/main/java/org/movsim/output/fileoutput/FileDetector.java
+++ src/main/java/org/movsim/output/fileoutput/FileDetector.java
@@ -16,11 +16,11 @@ public class FileDetector implements ObserverInTime {
 
     private static final String extensionFormat = ".det.road_%d.x_%d.csv";
     private static final String outputHeading = MovsimConstants.COMMENT_CHAR
-            + "   t[s],  v[km/h], rho[1/km],   Q[1/h],  nVeh,  Occup[1],1/<1/v>(km/h),<1/Tbrutto>(1/s)";
+            + "   t[s],  v[km/h], rho[1/km],   Q[1/h],   nVeh,  Occup[1],1/<1/v>[km/h],<1/Tbrutto>[1/s]";
 
     // note: number before decimal point is total width of field, not width of
     // integer part
-    private static final String outputFormat = "%8.1f, %8.3f, %8.3f, %8.1f, %8d, %8.7f, %8.3f, %8.5f%n";
+    private static final String outputFormat = "%8.1f, %8.3f, %8.3f, %8.1f, %8d, %8.7f, %8.3f, %8.5f, ";
 
     private PrintWriter printWriter = null;
     private final LoopDetector detector;
@@ -31,7 +31,7 @@ public class FileDetector implements ObserverInTime {
      * 
      * @param detector
      *            the detector
-     * @param laneCount 
+     * @param laneCount
      */
     public FileDetector(long roadId, LoopDetector detector, int laneCount) {
         final int xDetectorInt = (int) detector.getDetPosition();
@@ -50,35 +50,84 @@ public class FileDetector implements ObserverInTime {
     }
 
     /**
-     * Inits the file.
+     * Inits the printwriter and prints the header.
      * 
      * @param filename
      *            the filename
-     * @return the prints the writer
+     * @return the printWriter
      */
     private PrintWriter initFile(String filename) {
         printWriter = FileUtils.getWriter(filename);
+        printWriter.printf(MovsimConstants.COMMENT_CHAR + " number of lanes %d%n", laneCount);
         printWriter.printf(MovsimConstants.COMMENT_CHAR + " dtSample in s = %-8.4f%n", detector.getDtSample());
-        printWriter.printf(MovsimConstants.COMMENT_CHAR + " position xDet = %-8.4f%n", detector.getDetPosition());
+        printWriter.printf(MovsimConstants.COMMENT_CHAR + " position xDet in m = %-8.4f%n", detector.getDetPosition());
         printWriter.printf(MovsimConstants.COMMENT_CHAR + " arithmetic average for density rho%n");
-        printWriter.printf(outputHeading + "%n");
-        printWriter.flush();
+        printWriter.printf(MovsimConstants.COMMENT_CHAR + " All lanes");
+        printHeaderForAllLanes();
         return printWriter;
     }
 
+    private void printHeaderForAllLanes() {
+        for (int i = 0; i <= laneCount; i++) {
+            printWriter.printf(outputHeading);
+            if (i != laneCount) {
+                printWriter.printf(", lane: %d", i + 1);
+            }
+        }
+        printWriter.printf("%n");
+        printWriter.flush();
+    }
+
     /**
-     * Write aggregated data.
+     * Pulls data and writes aggregated data to output file.
      * 
      * @param time
      *            the time
      */
     private void writeAggregatedData(double time) {
-        for (int i = 0; i <laneCount; i++) {
-            printWriter.printf(outputFormat, time, 3.6 * detector.getMeanSpeed(i), 1000 * detector.getDensityArithmetic(i),
-                    3600 * detector.getFlow(i), detector.getVehCountOutput(i), detector.getOccupancy(i),
-                    3.6 * detector.getMeanSpeedHarmonic(i), detector.getMeanTimegapHarmonic(i));
-            printWriter.flush();
+        allLanesTogether(time);
+        perLane(time);
+        printWriter.printf("%n");
+        printWriter.flush();
+    }
+
+    /**
+     *  Writes out the values per lane.
+     * @param time
+     */
+    private void perLane(double time) {
+        for (int i = 0; i < laneCount; i++) {
+            printWriter.printf(outputFormat, time, 3.6 * detector.getMeanSpeed(i),
+                    1000 * detector.getDensityArithmetic(i), 3600 * detector.getFlow(i), detector.getVehCountOutput(i),
+                    detector.getOccupancy(i), 3.6 * detector.getMeanSpeedHarmonic(i),
+                    detector.getMeanTimegapHarmonic(i));
         }
+    }
+
+    /**
+     * Writes out the values over all lanes.
+     * 
+     * @param time
+     */
+    private void allLanesTogether(double time) {
+        double meanSpeed = 0;
+        double densityArithmetic = 0;
+        double flow = 0;
+        int vehCountOutput = 0;
+        double occupancy = 0;
+        double meanSpeedHarmonic = 0;
+        double meanTimegapHarmonic = 0;
+        for (int i = 0; i < laneCount; i++) {
+            meanSpeed += detector.getMeanSpeed(i);
+            densityArithmetic += detector.getDensityArithmetic(i);
+            flow += detector.getFlow(i);
+            vehCountOutput += detector.getVehCountOutput(i);
+            occupancy += detector.getOccupancy(i);
+            meanSpeedHarmonic += detector.getMeanSpeedHarmonic(i);
+            meanTimegapHarmonic += detector.getMeanTimegapHarmonic(i);
+        }
+        printWriter.printf(outputFormat, time, 3.6 * meanSpeed, 1000 * densityArithmetic, 3600 * flow, vehCountOutput,
+                occupancy, 3.6 * meanSpeedHarmonic, meanTimegapHarmonic);
     }
 
     /*
