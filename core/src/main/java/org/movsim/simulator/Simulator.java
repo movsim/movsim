04e87ea504d25f377e1eb8dd131a4a6e2fa430b7
ake <mail@akesting.de>
2012-02-24 21:21:58 +0100

    added warn message if road id from xml does not match to xodr network

--- core/src/main/java/org/movsim/simulator/Simulator.java
+++ core/src/main/java/org/movsim/simulator/Simulator.java
@@ -163,6 +163,10 @@ public class Simulator implements SimulationTimeStep, SimulationRun.CompletionCa
             final RoadSegment roadSegment = roadNetwork.findByUserId(roadinput.getId());
             if (roadSegment != null) {
                 addInputToRoadSegment(roadSegment, roadinput, vehGenerator);
+            } else {
+                // at least warn user that roadId cannot be matched to xodr roadnetwork
+                // TODO add option to exit here if user sets option. Such input errors are just annoying.
+                logger.warn("cannot find roadId={} from input in constructed roadNetwork. IGNORE DATA!!!");
             }
         }
     }
