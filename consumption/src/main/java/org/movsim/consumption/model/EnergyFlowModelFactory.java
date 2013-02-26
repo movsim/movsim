e3bf9809e607fb24a717d9c5c449fa4badcb9e76
ake <mail@akesting.de>
2013-02-26 17:10:23 +0100

    added class

---
+++ consumption/src/main/java/org/movsim/consumption/model/EnergyFlowModelFactory.java
@@ -0,0 +1,52 @@
+package org.movsim.consumption.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.movsim.consumption.autogen.ConsumptionModels;
+import org.movsim.consumption.autogen.Model;
+
+import com.google.common.base.Preconditions;
+
+public class EnergyFlowModelFactory {
+    
+    private final Map<String, EnergyFlowModel> energyFlowModels = new HashMap<>();
+    
+    public EnergyFlowModelFactory() {
+    }
+
+    public EnergyFlowModelFactory(ConsumptionModels models) {
+        initialize(models);
+    }
+
+    public void add(ConsumptionModels models) {
+        initialize(models);
+    }
+
+    private void initialize(ConsumptionModels models) {
+        Preconditions.checkNotNull(models);
+        Preconditions.checkArgument(models.isSetModel());
+        addModels(models.getModel());
+    }
+
+    /**
+     * Returns a cached {@link EnergyFlowModel} for the given {@code label} name.
+     * 
+     * @param label
+     * @return
+     */
+    public EnergyFlowModel get(String label) {
+        return energyFlowModels.get(label);
+    }
+
+    private void addModels(List<Model> models) {
+        for (Model model : models) {
+            if (energyFlowModels.containsKey(model.getLabel())) {
+                throw new IllegalArgumentException("consumption models do not have unique names (labels)!");
+            }
+            energyFlowModels.put(model.getLabel(), EnergyFlowModels.create(model));
+        }
+    }
+
+}
