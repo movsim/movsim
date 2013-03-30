6a0944f3015fdfa89abaa6ab0d64290282c98ead
ake <mail@akesting.de>
2013-03-30 16:53:38 +0100

    commented out sax parser

--- core/src/main/java/org/movsim/input/network/opendrive/OpenDriveReader.java
+++ core/src/main/java/org/movsim/input/network/opendrive/OpenDriveReader.java
@@ -7,14 +7,9 @@ import org.xml.sax.SAXException;
 
 public class OpenDriveReader {
 
-    final static boolean useJaxb = true; // true still for testing!
-
     public static boolean loadRoadNetwork(RoadNetwork roadNetwork, String fullXodrFileName) throws JAXBException,
             SAXException {
-        if (useJaxb) {
-            return OpenDriveHandlerJaxb.loadRoadNetwork(roadNetwork, fullXodrFileName);
-        }
-        return OpenDriveReaderSax.loadRoadNetwork(roadNetwork, fullXodrFileName);
+        return OpenDriveHandlerJaxb.loadRoadNetwork(roadNetwork, fullXodrFileName);
     }
 
 }
--- core/src/main/java/org/movsim/input/network/opendrive/OpenDriveReaderSax.java
+++ core/src/main/java/org/movsim/input/network/opendrive/OpenDriveReaderSax.java
@@ -1,50 +1,50 @@
-/*
- * Copyright (C) 2010, 2011, 2012 by Arne Kesting, Martin Treiber, Ralph Germ, Martin Budden
- *                                   <movsim.org@gmail.com>
- * -----------------------------------------------------------------------------------------
- * 
- * This file is part of
- * 
- * MovSim - the multi-model open-source vehicular-traffic simulator.
- * 
- * MovSim is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * MovSim is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with MovSim. If not, see <http://www.gnu.org/licenses/>
- * or <http://www.movsim.org>.
- * 
- * -----------------------------------------------------------------------------------------
- */
-
-package org.movsim.input.network.opendrive;
-
-import org.movsim.input.network.xml.XMLReaderBase;
-import org.movsim.simulator.roadnetwork.RoadNetwork;
-
-/**
- * Reads an OpenDRIVE format file and uses it to create a road network, see:
- * http://www.opendrive.org/docs/OpenDRIVEFormatSpecRev1.3D.pdf
- */
-@Deprecated
-public class OpenDriveReaderSax extends XMLReaderBase {
-    /**
-     * Reads an OpenDrive format file, creating a road network.
-     * 
-     * @param roadNetwork
-     * @param filename
-     * @return true if the road network file exists and was successfully parsed, false otherwise.
-     */
-    @Deprecated
-    public static boolean loadRoadNetwork(RoadNetwork roadNetwork, String filename) {
-        final OpenDriveHandlerSax handler = new OpenDriveHandlerSax(roadNetwork);
-        return parse(filename, handler);
-    }
-}
+// /*
+// * Copyright (C) 2010, 2011, 2012 by Arne Kesting, Martin Treiber, Ralph Germ, Martin Budden
+// * <movsim.org@gmail.com>
+// * -----------------------------------------------------------------------------------------
+// *
+// * This file is part of
+// *
+// * MovSim - the multi-model open-source vehicular-traffic simulator.
+// *
+// * MovSim is free software: you can redistribute it and/or modify
+// * it under the terms of the GNU General Public License as published by
+// * the Free Software Foundation, either version 3 of the License, or
+// * (at your option) any later version.
+// *
+// * MovSim is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// * See the GNU General Public License for more details.
+// *
+// * You should have received a copy of the GNU General Public License
+// * along with MovSim. If not, see <http://www.gnu.org/licenses/>
+// * or <http://www.movsim.org>.
+// *
+// * -----------------------------------------------------------------------------------------
+// */
+//
+// package org.movsim.input.network.opendrive;
+//
+// import org.movsim.input.network.xml.XMLReaderBase;
+// import org.movsim.simulator.roadnetwork.RoadNetwork;
+//
+// /**
+// * Reads an OpenDRIVE format file and uses it to create a road network, see:
+// * http://www.opendrive.org/docs/OpenDRIVEFormatSpecRev1.3D.pdf
+// */
+// @Deprecated
+// public class OpenDriveReaderSax extends XMLReaderBase {
+// /**
+// * Reads an OpenDrive format file, creating a road network.
+// *
+// * @param roadNetwork
+// * @param filename
+// * @return true if the road network file exists and was successfully parsed, false otherwise.
+// */
+// @Deprecated
+// public static boolean loadRoadNetwork(RoadNetwork roadNetwork, String filename) {
+// final OpenDriveHandlerSax handler = new OpenDriveHandlerSax(roadNetwork);
+// return parse(filename, handler);
+// }
+// }
