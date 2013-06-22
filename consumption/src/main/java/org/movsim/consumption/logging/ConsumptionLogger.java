9e5463e73e3235a80d463642adc61c0efa32cc79
ake <mail@akesting.de>
2013-06-22 19:03:54 +0200

    cleaned up log4j configs

--- consumption/src/main/java/org/movsim/consumption/logging/ConsumptionLogger.java
+++ consumption/src/main/java/org/movsim/consumption/logging/ConsumptionLogger.java
@@ -45,9 +45,12 @@ public class ConsumptionLogger {
         // Log Levels: DEBUG < INFO < WARN < ERROR;
         final File file = new File(LOG4J_PROPERTIES);
         if (file.exists() && file.isFile()) {
+            System.out.println("log4j configuration read from=" + file.getAbsolutePath());
             PropertyConfigurator.configure(LOG4J_PROPERTIES);
         } else {
             final URL log4jConfig = ConsumptionMain.class.getResource(LOG4J_PATH + LOG4J_PROPERTIES);
+            System.out.println("no log4j from commandline provided. Use default log4j configuration from="
+                    + log4jConfig.toExternalForm());
             PropertyConfigurator.configure(log4jConfig);
         }
     }
