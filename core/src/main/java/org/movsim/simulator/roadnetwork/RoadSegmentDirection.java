ade65994cb73c0569c8149cef4670857b4b92da6
ake <mail@akesting.de>
2013-07-17 19:03:03 +0200

    added missing class

---
+++ core/src/main/java/org/movsim/simulator/roadnetwork/RoadSegmentDirection.java
@@ -0,0 +1,15 @@
+package org.movsim.simulator.roadnetwork;
+
+/**
+ * Type of direction of a directed edge. A backward edge is also called 'peer'.
+ * 
+ * 
+ * <br>
+ * created: Jul 17, 2013<br>
+ * 
+ */
+public enum RoadSegmentDirection {
+
+    FORWARD, BACKWARD;
+
+}
