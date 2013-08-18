7b85e0215b8e2bcb63932693aebdcde6dd5c6f9e
ake <mail@akesting.de>
2013-08-18 14:53:30 +0200

    removed obsolete external impl of trafficlight

--- core/src/main/java/org/movsim/simulator/roadnetwork/controller/TrafficLightControllerFactory.java
+++ core/src/main/java/org/movsim/simulator/roadnetwork/controller/TrafficLightControllerFactory.java
@@ -5,9 +5,6 @@ import org.movsim.autogen.ControllerGroup;
 public final class TrafficLightControllerFactory {
 
     static TrafficLightController create(ControllerGroup controllerGroup) {
-        if (controllerGroup.isSetExternal()) {
-            return new TrafficLightControllerExternal(controllerGroup);
-        }
         return new TrafficLightControllerInternal(controllerGroup);
     }
 
