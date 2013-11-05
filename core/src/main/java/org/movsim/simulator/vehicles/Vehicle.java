2d41c35a9e2a23cabe957a52697813adb9c71c3d
akegermany <mail@akesting.de>
2013-11-05 14:30:52 +0100

    added debug output

--- core/src/main/java/org/movsim/simulator/vehicles/Vehicle.java
+++ core/src/main/java/org/movsim/simulator/vehicles/Vehicle.java
@@ -487,6 +487,7 @@ public class Vehicle {
     }
 
     public final double getEffectiveSpeedlimit() {
+        LOG.debug("speedlimit={}, maxAllowedSpeedOnRoad={}", speedlimit, getMaxAllowedSpeedOnRoad());
         return Math.min(speedlimit, getMaxAllowedSpeedOnRoad());
     }
     
