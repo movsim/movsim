a6605e6ae7c0c75d089fa5e3400347700c403357
akegermany <mail@akesting.de>
2015-03-10 13:35:47 +0100

    implemented adding and removal of vehicles

--- core/src/main/java/org/movsim/simulator/vehicles/ExternalVehiclesController.java
+++ core/src/main/java/org/movsim/simulator/vehicles/ExternalVehiclesController.java
@@ -5,73 +5,147 @@ import generated.MovsimExternalVehicleControl;
 import generated.SpeedDataType;
 import generated.VehicleUserDataType;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
+import org.movsim.simulator.roadnetwork.LaneSegment;
 import org.movsim.simulator.roadnetwork.RoadNetwork;
 import org.movsim.simulator.roadnetwork.RoadSegment;
 import org.movsim.utilities.TimeUtilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.TreeMultimap;
 
 public class ExternalVehiclesController {
 
-    /** time-sorted map of vehicle input data */
-    private TreeMultimap<Double, ExternalVehicleType> vehiclesToAdd;
-    
+    private static final Logger LOG = LoggerFactory.getLogger(ExternalVehiclesController.class);
+
+    /** time-sorted map of external vehicle input data */
+    private final SortedMap<Double, List<ExternalVehicleType>> externalVehicleInputsToAdd = new TreeMap<>();
+
     /** time-sorted map of vehicles to remove */
-    private TreeMultimap<Long, Vehicle> vehiclesToRemove;
-    
-    
+    private final SortedMap<Double, List<Vehicle>> externalVehiclesToRemove = new TreeMap<>();
+
     private final Map<Vehicle, List<SpeedDataType>> controlledVehicles = new HashMap<>();
 
-    // TODO info for roadSegment not yet stored
-    
-    private void createVehicles(MovsimExternalVehicleControl input) {
-        for (ExternalVehicleType vehicleInput : input.getExternalVehicle()) {
-            Preconditions.checkArgument(!vehicleInput.getSpeedData().isEmpty(), "external vehicle needs at least one speed-data entry");
-            String time = vehicleInput.getSpeedData().get(0).getTime();
-            double timestamp = TimeUtilities.convertToSeconds(time, input.getTimeFormat());
-            // TODO timestamp key for sorted mapping to vehicles 
-            
-            double initialSpeed = vehicleInput.getSpeedData().get(0).getSpeed();
-            Vehicle vehicle = new Vehicle(vehicleInput.getPosition(), initialSpeed, vehicleInput.getLane(),
-                    vehicleInput.getLength(), vehicleInput.getWidth());
-            vehicle.setType(Vehicle.Type.EXTERNAL_CONTROL);
-
-            for (VehicleUserDataType userData : vehicleInput.getVehicleUserData()) {
-                vehicle.getUserData().put(userData.getKey(), userData.getValue());
-            }
-
-            controlledVehicles.put(vehicle, vehicleInput.getSpeedData()); // time series data
-            
-        }
-    }
+    private String timeFormat;
 
     public void setInput(MovsimExternalVehicleControl input) {
         Preconditions.checkNotNull(input);
-        createVehicles(input);
+        this.timeFormat = input.getTimeFormat();
+        createExternalVehicleData(input);
     }
 
-    
-    public void addRemoveVehicles(double simulationTime, RoadNetwork roadNetwork) {
-        if (simulationTime <= 0) {
-            for (Vehicle veh : controlledVehicles.keySet()) {
-                // just for testing  
-                roadNetwork.iterator().next().addVehicle(veh);
-            }
-        }
-    }
-
-    public void setVehicleSpeeds(double simulationTime, RoadSegment roadSegment) {
-        //testwise
+    /**
+     * sets the speeds of externally controlled vehicles in whole road network
+     * @param simulationTime
+     * @param roadSegment
+     */
+    public void setSpeeds(double simulationTime) {
         for (Vehicle veh : controlledVehicles.keySet()) {
             veh.setSpeed(5);
         }
-        
     }
 
-    // TODO time series mit interpolation, analog zu InflowTimeSeries ... etwas generisches!
+    /**
+     * adds and removes externally controlled vehicles as configured in the simulation input.
+     * @param simulationTime
+     * @param roadNetwork
+     */
+    public void addAndRemoveVehicles(double simulationTime, RoadNetwork roadNetwork) {
+        addVehiclesToRoadNetwork(simulationTime, roadNetwork);
+        removeVehiclesFromRoadNetwork(simulationTime, roadNetwork);
+    }
+
+    private void createExternalVehicleData(MovsimExternalVehicleControl input) {
+        for (ExternalVehicleType externalVehicleData : input.getExternalVehicle()) {
+            Preconditions.checkArgument(!externalVehicleData.getSpeedData().isEmpty(),
+                    "external vehicle needs at least one (time, speed) data entry");
+            // no check if (time, speed) input data is sorted in time
+            String entryTime = externalVehicleData.getSpeedData().get(0).getTime();
+            double entryTimestamp = TimeUtilities.convertToSeconds(entryTime, timeFormat);
+            addVehicleInput(entryTimestamp, externalVehicleData);
+        }
+    }
+
+    private void addVehiclesToRoadNetwork(double simulationTime, RoadNetwork roadNetwork) {
+        while (!externalVehicleInputsToAdd.isEmpty() && externalVehicleInputsToAdd.firstKey() <= simulationTime) {
+            Double firstKey = externalVehicleInputsToAdd.firstKey();
+            List<ExternalVehicleType> externalVehicles = externalVehicleInputsToAdd.remove(firstKey);
+            addVehiclesToRoadSegments(externalVehicles, roadNetwork);
+            LOG.debug("added {} external vehicles to roadNetwork, removed entries for time={}", externalVehicles.size(),
+                    firstKey);
+        }
+    }
+
+    private void removeVehiclesFromRoadNetwork(double simulationTime, RoadNetwork roadNetwork) {
+        while (!externalVehiclesToRemove.isEmpty() && simulationTime >= externalVehiclesToRemove.firstKey()) {
+            Double firstKey = externalVehiclesToRemove.firstKey();
+            List<Vehicle> externalVehicles = externalVehiclesToRemove.remove(firstKey);
+            removeVehiclesFromRoadSegments(externalVehicles, roadNetwork);
+        }
+    }
+
+    private void removeVehiclesFromRoadSegments(List<Vehicle> externalVehicles, RoadNetwork roadNetwork) {
+        for (Vehicle vehicle : externalVehicles) {
+            int roadSegmentId = vehicle.roadSegmentId();
+            RoadSegment roadSegment = roadNetwork.findById(roadSegmentId);
+            LaneSegment laneSegment = roadSegment.laneSegment(vehicle.lane());
+            laneSegment.removeVehicle(vehicle);
+            LOG.info("removed externally controlled vehicle={} from roadSegment={}", vehicle, roadSegment);
+        }
+    }
+
+    private boolean addVehicleInput(double timestamp, ExternalVehicleType externalVehicleInput) {
+        List<ExternalVehicleType> externalVehicles = externalVehicleInputsToAdd.get(timestamp);
+        if (externalVehicles == null) {
+            externalVehicles = new ArrayList<ExternalVehicleType>();
+            externalVehicleInputsToAdd.put(timestamp, externalVehicles);
+        }
+        return externalVehicles.add(externalVehicleInput);
+    }
+
+    private boolean addVehicleToRemoveMap(double timestamp, Vehicle vehicle) {
+        List<Vehicle> vehiclesToRemove = externalVehiclesToRemove.get(timestamp);
+        if (vehiclesToRemove == null) {
+            vehiclesToRemove = new ArrayList<Vehicle>();
+            externalVehiclesToRemove.put(timestamp, vehiclesToRemove);
+        }
+        return vehiclesToRemove.add(vehicle);
+    }
+
+    private void addVehiclesToRoadSegments(List<ExternalVehicleType> vehicleInputs, RoadNetwork roadNetwork) {
+        for (ExternalVehicleType vehicleInput : vehicleInputs) {
+            Vehicle vehicle = createVehicle(vehicleInput);
+            String roadId = vehicleInput.getRoadId();
+            RoadSegment roadSegment = roadNetwork.findByUserId(roadId);
+            Preconditions.checkNotNull(roadSegment, "cannot find roadSegment with id=" + roadId + " for external vehicle");
+            roadSegment.addVehicle(vehicle);
+            controlledVehicles.put(vehicle, vehicleInput.getSpeedData());
+            LOG.info("added externally controlled vehicle={} to roadSegment={}", vehicle, roadSegment);
+
+            // and add vehicle to removal container
+            int size = vehicleInput.getSpeedData().size();
+            String exitTime = vehicleInput.getSpeedData().get(size - 1).getTime();
+            double exitTimestamp = TimeUtilities.convertToSeconds(exitTime, timeFormat);
+            addVehicleToRemoveMap(exitTimestamp, vehicle);
+            LOG.info("external vehicle={} will be removed at timestamp={}", vehicle, exitTimestamp);
+        }
+    }
+
+    private Vehicle createVehicle(ExternalVehicleType data) {
+        double initialSpeed = data.getSpeedData().get(0).getSpeed();
+        Vehicle vehicle = new Vehicle(data.getPosition(), initialSpeed, data.getLane(), data.getLength(), data.getWidth());
+        vehicle.setType(Vehicle.Type.EXTERNAL_CONTROL);
+        for (VehicleUserDataType userData : data.getVehicleUserData()) {
+            vehicle.getUserData().put(userData.getKey(), userData.getValue());
+        }
+        return vehicle;
+    }
+
 }
