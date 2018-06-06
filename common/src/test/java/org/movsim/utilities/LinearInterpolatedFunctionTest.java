64dbb10dbf80e71bdfaa4e1fb28e39ffba3b2d58
akegermany <mail@akesting.de>
2018-06-06 17:40:15 +0200

    updated dependencies to latest versions, avoided  usage of deprecated methods, switched to Java version 8 as default as required by a dependency relying on default methods in interfaces

--- common/src/test/java/org/movsim/utilities/LinearInterpolatedFunctionTest.java
+++ common/src/test/java/org/movsim/utilities/LinearInterpolatedFunctionTest.java
@@ -1,7 +1,7 @@
 package org.movsim.utilities;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.Test;
 
